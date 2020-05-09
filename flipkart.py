2a6a0f6222d8816586dd7f5010f77ac1fa6d3243
itibera <itibera5@gmail.com>
2020-05-09 18:46:35 +0530

    run successfully

---
+++ flipkart.py
@@ -0,0 +1,29 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from pandas import DataFrame
+
+product = input("Enter the Product: ")
+product = product.replace(' ', '%20')
+
+urlname = "https://www.flipkart.com/search?q=" + product + "&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off"
+print(urlname)
+try:
+    page = urlopen(urlname)
+except:
+    print('Error in page loading..')
+soup = BeautifulSoup(page, 'html.parser')
+#print(soup)
+
+products = []
+prices = []
+for i in soup.findAll('div', {'class': '_3O0U0u'}):
+    #print(i)
+    name = i.find('div', {'class': '_3wU53n'})
+
+    price=i.find('div',{'class':'_1vC4OE _2rQ-NK'})
+    products.append(name)
+    prices.append(price)
+    #print(i.find('div', {'class': '_1HmYoV _35HD7C'}))
+
+df=DataFrame({'Products':products,'Prices':prices})
+print(df)
